ba27a6892e48cce531cc965efc34e5381520dcb9
Hazal Onler <hazaltaneri@gmail.com>
2023-07-24 21:14:01 -0400

    Dataclasses were added for board_controller

--- src/app.py
+++ src/app.py
@@ -1,8 +1,10 @@
 from flask import render_template
+from flask_cors import CORS
 import connexion
 
 app = connexion.App(__name__, specification_dir="./")
 app.add_api("swagger.yml")
+cors = CORS(app.app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route("/")
--- src/controller/board_controller.py
+++ src/controller/board_controller.py
@@ -2,49 +2,36 @@ from dataclasses import dataclass
 
 
 @dataclass
+class Style:
+    background_color: str
+    border_radius: str
+    width: str
+
+
+@dataclass
+class ListConfig:
+    id: str
+    name: str
+    style: Style
+
+
+@dataclass
 class BoardConfig:
     board_name: str
     board_id: str
-    list_config: list
+    list_config: list[ListConfig]
 
 
-BOARD_CONFIG = BoardConfig("Hazal's Project", "a1", [
-            {
-                "id": 'e1',
-                "name": "Backlog",
-                "style": {
-                    "backgroundColor": "rgb(255, 204, 204)",
-                    "borderRadius": "12px",
-                    "width": "230px"
-                }
-            },
-            {
-                "id": 'e2',
-                "name": "To-Do",
-                "style": {
-                    "backgroundColor": "rgb(255, 204, 204)",
-                    "borderRadius": "12px",
-                    "width": "230px"
-                }
-            },
-            {
-                "id": 'e3',
-                "name": "In-Progress",
-                "style": {
-                    "backgroundColor": "rgb(255, 204, 204)",
-                    "borderRadius": "12px",
-                    "width": "230px"
-                }
-            },
-            {
-                "id": 'e4',
-                "name": "Done",
-                "style": {
-                    "backgroundColor": "rgb(255, 204, 204)",
-                    "borderRadius": "12px",
-                    "width": "230px"}
-            },
-        ])
+style_config = Style("rgb(255, 204, 204)", "12px", "230px")
+
+
+listConfig_1 = ListConfig("e1", "Backlog", style_config)
+listConfig_2 = ListConfig("e2", "To-Do", style_config)
+listConfig_3 = ListConfig("e3", "In-Progress", style_config)
+listConfig_4 = ListConfig("e4", "Done", style_config)
+
+
+BOARD_CONFIG = BoardConfig("Hazal's Project", "a1", [listConfig_1, listConfig_2, listConfig_3, listConfig_4])
 
 
 def read_board(boardId):
